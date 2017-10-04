75e43edb23394808b6d2f20f74c3e39055436201
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-04 12:48:29 +1100

    Add 1.2

--- Playful.playground/Pages/1.2.xcplaygroundpage/Contents.swift
+++ Playful.playground/Pages/1.2.xcplaygroundpage/Contents.swift
@@ -6,15 +6,47 @@ import Foundation
  Check Permutation: Given two string, write a method to decide if one is a permutation of the other.
  */
 
-func test() -> Bool {
-  return true
+// We could use the `sorted()` method on a string, but let's build our own string sort function.
+
+func sort(input: [Character]) -> [Character] {
+  guard let sentinel = input.first else { return input }
+  let restInput = input.suffix(from: 1)
+
+  var lessThan: Array<Character> = []
+  var greaterThanEqual: Array<Character> = []
+
+  for char in restInput {
+    if char < sentinel {
+      lessThan.append(char)
+    } else {
+      greaterThanEqual.append(char)
+    }
+  }
+
+  var results: Array<Character> = []
+  results += sort(input: lessThan)
+  results.append(sentinel)
+  results += sort(input: greaterThanEqual)
+
+  return results
 }
 
-let result = test()
+let sortTest = sort(input: Array("dcbea"))
+print(sortTest)
+
+
+
+func checkPermutation(input1: String, input2: String) -> Bool {
+  //return input1.sorted() == input2.sorted()
+  return sort(input: Array(input1)) == sort(input: Array(input2))
+}
+
+let result = checkPermutation(input1: "abc", input2: "cba")
 print(result)
 
 /* Discussion:
- ...
+ The easiest way is to sort the input strings (characters) and compare them. In other words, we just check for the distinct characters and the numbers of each character. And disregarding the order.
+ The runtime complexity is O(n log n) for sorting one input string and O(n) for comparing those 2 strings (we need to iterate the characters in the string).
  */
 
 //: [Next](@next)
