31cddd08fad06d88315a94586f0c1720b5673f61
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-05 08:27:23 +1100

    Add 1.3

--- Playful.playground/Pages/1.3.xcplaygroundpage/Contents.swift
+++ Playful.playground/Pages/1.3.xcplaygroundpage/Contents.swift
@@ -9,15 +9,33 @@ import Foundation
  Output: "Mr%20John%20Smith"
  */
 
-func test() -> Bool {
-  return true
+func urlify(input: inout String, length: Int) -> String {
+  var charArray = Array(input)
+  var indexOffset = 0
+
+  for (index, char) in charArray.prefix(length).enumerated() {
+    if char == " " {
+      let realIndex = index + indexOffset
+      charArray[realIndex] = "%"
+      charArray.insert("2", at: realIndex.advanced(by: 1))
+      charArray.insert("0", at: realIndex.advanced(by: 2))
+      indexOffset += 2
+    }
+  }
+
+  input = String(charArray).trimmingCharacters(in: [" "])
+
+  return input
 }
 
-let result = test()
+var input = "Mr John Smith    "
+let result = urlify(input: &input, length: 13)
 print(result)
+print(result.count)
 
 /* Discussion:
- ...
+ Given that it says that the string has sufficient space, I guess it means that we need to perform the replacement in place.
+ Runtime complexity is O(n) and no additional space is required. The trick is to keep indexOffset since we are performing the replacement in place whilst the index given by enumerated() refers to the space (" ") index in the old string before replacement.
  */
 
 //: [Next](@next)
