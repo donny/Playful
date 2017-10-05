e56fab571fb58c7e2c80c1111f1de7ddb114f901
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-06 08:14:29 +1100

    Add 1.4

--- Playful.playground/Pages/1.4.xcplaygroundpage/Contents.swift
+++ Playful.playground/Pages/1.4.xcplaygroundpage/Contents.swift
@@ -9,15 +9,33 @@ import Foundation
  Output: True (permutations: "taco cat", "atco cta", etc.)
  */
 
-func test() -> Bool {
-  return true
+func palindromePermutation(input: String) -> Bool {
+  let charArray = Array(input.lowercased().replacingOccurrences(of: " ", with: ""))
+  var charDict: Dictionary<Character, Bool> = [:]
+  // The charDict value is true if we have encountered the character odd-numbered times.
+  // The charDict value is false if we have encountered the character even-numbered times.
+
+  // Store the number of each character in the string.
+  for char in charArray {
+    if let value = charDict[char] {
+      charDict[char] = !value
+    } else {
+      charDict[char] = true // First time that we encounter the character.
+    }
+  }
+
+  // Check the number of each character.
+  let newDict = charDict.filter { (char, isOdd) -> Bool in return isOdd }
+
+  return (newDict.count == 1 || newDict.count == 0)
 }
 
-let result = test()
+let result = palindromePermutation(input: "Tact Coa")
 print(result)
 
 /* Discussion:
- ...
+ A palindrome can have at most one character that is odd-numbered in total. This algorithm maintains a lookup table and parses the character one by one.
+ The runtime complexity is O(n).
  */
 
 //: [Next](@next)
