f7f93f27d656c8a678da66334431fc9a9d1d4d2b
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-06 12:59:10 +1100

    Solve 1.5

--- Playful.playground/Pages/1.5.xcplaygroundpage/Contents.swift
+++ Playful.playground/Pages/1.5.xcplaygroundpage/Contents.swift
@@ -11,15 +11,55 @@ import Foundation
  pale, bake -> false
  */
 
-func test() -> Bool {
+func oneAway(input1: String, input2: String) -> Bool {
+  let count1 = input1.count
+  let count2 = input2.count
+  let maxCount = max(count1, count2)
+  var oneEditAlready = false
+
+  var count1Offset = 0
+  var count2Offset = 0
+
+  if abs(count1 - count2) > 1 { return false }
+
+  for i in 0..<maxCount {
+    let input1Index = input1.index(input1.startIndex, offsetBy: i + count1Offset)
+    let input2Index = input2.index(input2.startIndex, offsetBy: i + count2Offset)
+
+    if input1Index >= input1.endIndex || input2Index >= input2.endIndex {
+      if oneEditAlready { return false }
+      oneEditAlready = true
+      continue
+    }
+
+    let input1Char = input1[input1Index]
+    let input2Char = input2[input2Index]
+
+    if input1Char == input2Char { continue }
+
+    // The characters are different.
+    if oneEditAlready { return false }
+    oneEditAlready = true
+
+    if count1 == count2 {
+      continue
+    } else if count1 > count2 {
+      count2Offset = -1
+    } else if count1 < count2 {
+      count1Offset = -1
+    }
+  }
+
   return true
 }
 
-let result = test()
-print(result)
+print(oneAway(input1: "pale", input2: "ple"))
+print(oneAway(input1: "pales", input2: "pale"))
+print(oneAway(input1: "pale", input2: "bale"))
+print(oneAway(input1: "pale", input2: "bake"))
 
 /* Discussion:
- ...
+ The runtime complexity is O(n). I'm not happy with this solution. It seems very convoluted.
  */
 
 //: [Next](@next)
