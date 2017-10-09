c417bac345b185576343972da6642625bfab5b54
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-09 14:50:33 +1100

    Work in progress

--- Playful.playground/Pages/1.7.xcplaygroundpage/Contents.swift
+++ Playful.playground/Pages/1.7.xcplaygroundpage/Contents.swift
@@ -6,16 +6,34 @@ import Foundation
  Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
  */
 
-func test() -> Bool {
-  return true
+func rotateMatrix(input: [[Int]]) -> [[Int]]? {
+  // Check to ensure that it's an NxN matrix.
+  var matrixCount = input.count
+  for row in input {
+    if row.count != matrixCount {
+      return nil
+    }
+  }
+  var output = input
+
+  for i in 0..<(matrixCount-1) {
+    // Swap 0,i with the rightmost column j,(matrixCount-1)
+    (output[0][i], output[i][matrixCount-1]) = (output[i][matrixCount-1], output[0][i])
+  }
+
+  print(output)
+
+  return input
 }
 
-let result = test()
-print(result)
+if let result = rotateMatrix(input: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]) {
+  print(result)
+} else {
+  print("Error")
+}
 
 /* Discussion:
- ...
-
+ We could start by looking at a few small examples below:
 
  Inner edge:
  ab -> ba -> da -> ca
@@ -28,6 +46,20 @@ print(result)
  ijkl    ijkc    ijkc    ojkc
  mnop    mnop    mlhd    plhd
 
+ And, it can be done in place:
+
+ (1)    (2)    (3)    (4)
+ abc -> cfa -> ih. -> gd.
+ def    ..b    ...    h..
+ ghi    ...    .fc    i..
+
+ We just need to:
+ - swap the top row with the rightmost column
+ - swap the top row with the bottom row
+ - swap the top row with the leftmost column
+ (and do it for the inner (N-1)x(N-1) matrix)
+
+ We assume that the input is an array of array of characters / numbers.
  */
 
 //: [Next](@next)
