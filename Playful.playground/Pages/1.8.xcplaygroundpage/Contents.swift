22c6ccf32e99d53700c84ff04ce89432b9f44fa9
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-11 08:10:24 +1100

    Add answer for 1.8

--- Playful.playground/Pages/1.8.xcplaygroundpage/Contents.swift
+++ Playful.playground/Pages/1.8.xcplaygroundpage/Contents.swift
@@ -8,16 +8,31 @@ import Foundation
 
 func zeroMatrix(input: [[Int]]) -> [[Int]]? {
   // Check the size for correctness.
-  var matrixCount: Int?
+  let rowCount = input.count
+  var colCount: Int?
   for i in input {
-    if let count = matrixCount {
+    if let count = colCount {
       if i.count != count { return nil }
     } else {
-      matrixCount = i.count
+      colCount = i.count
+    }
+  }
+  var output = input
+
+  for i in 0..<rowCount {
+    for j in 0..<colCount! {
+      if input[i][j] == 0 {
+        for ix in 0..<rowCount {
+          output[ix][j] = 0
+        }
+        for jx in 0..<colCount! {
+          output[i][jx] = 0
+        }
+      }
     }
   }
 
-  return input
+  return output
 }
 
 func printMatrix(_ input: [[Int]]) -> Void {
@@ -26,25 +41,39 @@ func printMatrix(_ input: [[Int]]) -> Void {
   }
 }
 
-if let result = zeroMatrix(input: [[1, 2], [3, 4]]) {
+var input = [[1, 2, 3], [4, 5, 6]]
+printMatrix(input)
+print("--")
+if let result = zeroMatrix(input: input) {
   printMatrix(result)
 } else {
   print("Error")
 }
 
-if let result = zeroMatrix(input: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]) {
+print("------")
+
+input = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+printMatrix(input)
+print("--")
+if let result = zeroMatrix(input: input) {
   printMatrix(result)
 } else {
   print("Error")
 }
 
-if let result = zeroMatrix(input: [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]) {
+print("------")
+
+input = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+printMatrix(input)
+print("--")
+if let result = zeroMatrix(input: input) {
   printMatrix(result)
 } else {
   print("Error")
 }
+
 /* Discussion:
- ...
+ Runtime complexity is O(M x N) for the best case scenario where there is no zero. Otherwise, it's O(M x N x (M+N)) when all of them is zero.
  */
 
 //: [Next](@next)
