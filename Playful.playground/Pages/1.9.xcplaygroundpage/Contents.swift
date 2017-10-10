fc6906cd532104c197d728467916ef75071d55a7
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-11 08:19:53 +1100

    Add answer to 1.9

--- Playful.playground/Pages/1.9.xcplaygroundpage/Contents.swift
+++ Playful.playground/Pages/1.9.xcplaygroundpage/Contents.swift
@@ -6,15 +6,16 @@ import Foundation
  String Rotation: Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only once call to isSubstring (e.g., "waterbottle" is a rotation of "erbottlewat").
  */
 
-func test() -> Bool {
-  return true
+func stringRotation(input1: String, input2: String) -> Bool {
+  let combined = input1 + input1
+  return combined.contains(input2)
 }
 
-let result = test()
+let result = stringRotation(input1: "erbottlewat", input2: "waterbottle")
 print(result)
 
 /* Discussion:
- ...
+ Because it's a rotation, we could just combine input1 with itself, and check for the presence of input2.
  */
 
 //: [Next](@next)
